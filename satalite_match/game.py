e09d6580325307c9363214deeeef60f16559ea36
JochemJetLearn <jochemdebree0@gmail.com>
2026-01-13 17:27:31 +0100

    Add files via upload

--- satalite_match/game.py
+++ satalite_match/game.py
@@ -6,7 +6,12 @@ HEIGHT = 600
 sattalites = []
 satt_num = 8
 
+lines = []
+
+clicked_num = 0
+
 start_time = time.time()
+end_time = False
 
 def create_sattalites():
     for i in range(satt_num):
@@ -20,8 +25,24 @@ def draw():
     for num in range(1, len(sattalites)+1):
         sattalites[num-1].draw()
         screen.draw.text(str(num), (sattalites[num-1].x, sattalites[num-1].y+15), color="white")
+    for i in lines:
+        screen.draw.line(i[0], i[1], "white")
+    if end_time != False:
+        total = end_time - start_time
+        screen.draw.text(f"Run completed in {round(total, 2)} seconds!", (25, 25), color="white")
 
 def on_mouse_down(pos):
+    global clicked_num, lines, end_time
+    if clicked_num < satt_num:
+        if sattalites[clicked_num].collidepoint(pos):
+            if clicked_num:
+                lines.append((sattalites[clicked_num].pos, sattalites[clicked_num-1].pos))
+            clicked_num += 1
+        else:
+            lines = []
+            clicked_num = 0
+    if clicked_num == satt_num:
+        end_time = time.time()
 
 create_sattalites()
 
