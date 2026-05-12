b5b8b619764d8db29bb9cd9ce6dab867d620ec8c
JochemJetLearn <jochemdebree0@gmail.com>
2026-05-12 17:09:30 +0200

    Add files via upload

--- galaga2/gamev2.py
+++ galaga2/gamev2.py
@@ -3,6 +3,10 @@ import pgzrun, time, pygame
 WIDTH = 500
 HEIGHT = 750
 
+shipspeed = 5
+bulletsspeed = 5
+bugspeed = 1
+
 score = 0
 
 ship = Actor('ship')
@@ -11,8 +15,28 @@ ship.pos = (WIDTH // 2, HEIGHT - 50)
 lose = False
 win = False
 
+starttime = time.time()
+endtime = 0
+
 bugs = []
 bullets = []
+
+def reset():
+    global score, lose, win, starttime, bugs, bullets, WIDTH, HEIGHT, height
+    starttime = time.time()
+    if win:
+        WIDTH += 100
+    if lose:
+        score = 0
+        WIDTH = 500
+        HEIGHT = 750
+    screen.surface = pygame.display.set_mode((WIDTH, HEIGHT))
+    bugs = []
+    bullets = []
+    display_bugs()
+    lose = False
+    win = False
+    
 
 def display_bugs():
     for x in range(WIDTH // 100):
@@ -22,16 +46,17 @@ def display_bugs():
             bugs.append(bug)
 
 def update():
-    global score, lose, win
+    global score, lose, win, endtime
     if lose or win:
         return
     if keyboard.left and 50 < ship.x:
-        ship.x -= 5
+        ship.x -= shipspeed
     if keyboard.right and ship.x < WIDTH-50:
-        ship.x += 5
+        ship.x += shipspeed
 
     if len(bugs) == 0:
         win = True
+        endtime = time.time()
     
     for i in bugs:
         i.y += 1
@@ -39,7 +64,7 @@ def update():
             lose = True
 
     for bullet in bullets:
-        bullet.y -= 5
+        bullet.y -= bulletsspeed
         for i in bugs:
             if i.colliderect(bullet):
                 try:
@@ -56,10 +81,12 @@ def draw():
     screen.clear()
     ship.draw()
     if lose:
-        screen.draw.text(f"Game Over, Score: {score}", center=(WIDTH//2, HEIGHT//2), fontsize=50, color="red")
+        screen.draw.text(f"Game Over, Score: {score}, Level {WIDTH // 100-4}", center=(WIDTH//2, HEIGHT//2), fontsize=50, color="red")
+        screen.draw.text(f"Press R to reset", center=(WIDTH//2, HEIGHT//2+100), fontsize=50, color="red")
         return
     if win:
-        screen.draw.text(f"You Win!", center=(WIDTH//2, HEIGHT//2), fontsize=50, color="green")
+        screen.draw.text(f"You Win! Time: {round(endtime-starttime, 2)}s, Level {WIDTH // 100-4} ({score} Pt.)", center=(WIDTH//2, HEIGHT//2), fontsize=50, color="green")
+        screen.draw.text(f"Press R to play the next level", center=(WIDTH//2, HEIGHT//2+100), fontsize=50, color="green")
         return
     for i in bugs:
         i.draw()
@@ -72,5 +99,7 @@ def on_key_down(key):
         bullet = Actor("bullet.png")
         bullet.pos = (ship.x, HEIGHT - 50)
         bullets.append(bullet)
+    if key == keys.R and (lose or win):
+        reset()
 
 pgzrun.go()
